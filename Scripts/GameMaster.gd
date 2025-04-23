07b11eb0f8f4ec1cc56b15a5a3d451a64adca3be
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2025-04-22 21:26:18 -0400

    Mo' files

---
+++ Scripts/GameMaster.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var timer: int = 0
+var shots: int = 0
+var hits: int = 0
+var lvl_current: int = 0
+var lvl_cleared: int = 0
---
+++ Scripts/Level.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var complete: bool = false
+var targets: Array = []
+var target_order: bool = false #if it matters the order the targets are hit
