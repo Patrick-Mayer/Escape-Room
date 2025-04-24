594b63e3d2a44cb25cbe9c139349c53a4c1e6b0a
Patrick-Mayer <53142589+Patrick-Mayer@users.noreply.github.com>
2025-04-24 00:14:44 -0400

    we got everything but animations

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -3,16 +3,43 @@ extends XRToolsPickable
 class_name Gun
 
 @export var casingPrefab : PackedScene;
+@export var magazinePrefab : PackedScene;
+
+@export var animator : AnimationPlayer;
+@export var shootSFX : AudioStreamPlayer3D;
+@export var clickSFX : AudioStreamPlayer3D;
+
+
+
+var currentMagazine : Magazine;
+
+
+func _ready():
+	var currentMagazine = magazinePrefab.instantiate() as Magazine;
+
 
 func Shoot():
-	EjectCasing();
+	if animator.is_playing():
+		return;
 	
+	if (currentMagazine.HasAmmo()):
+		#fire projectile
+		shootSFX.play();
+		EjectCasing();
+		currentMagazine.ammo -= 1;
+	else:
+		clickSFX.play();
+		
 	
 func EjectCasing():
 	if casingPrefab != null:
 		var tempPrefab = casingPrefab.instantiate() as Casing;
-		tempPrefab.transform = $Pistol/Pistol/slide/CasingSpawn.global_transform;
 		#adds it to our scene
 		add_child(tempPrefab);
 		
-		tempPrefab.linear_velocity = tempPrefab.transform.basis.y;
+		tempPrefab.transform = $Pistol/Pistol/slide/CasingSpawn.global_transform;
+		tempPrefab.linear_velocity = tempPrefab.transform.basis.y * 10.0;
+
+#this is how we handle trigger input on VR controller
+func action():
+	pass
