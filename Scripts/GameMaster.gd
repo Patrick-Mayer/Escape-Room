99dd51038a4a7513e5293b3ff23969596b8d79f9
Patrick-Mayer <53142589+Patrick-Mayer@users.noreply.github.com>
2025-04-28 10:12:24 -0400

    added tutorial text for first NextLevelTarget

--- Scripts/GameMaster.gd
+++ Scripts/GameMaster.gd
@@ -1,6 +1,5 @@
 #Last minute things to do:
 #2. Make level with moving targets (not circles though, that's an AL job if he wants to try and cram that in there last min)
-#4. Timer functionality (although it may already be working in the code)
 #5. Tutorial text over NextLevelTarget
 #6. Audiosource SFX for hitting target
 #7. Export project
